99705115291115b3ae3fa7f49409a6193709f0ba
Никита Ларин <l_nikita2000@mail.ru>
2023-07-04 16:34:44 +0700

    модальное окно для загрузки файлов

--- client/src/components/ChatInput/index.jsx
+++ client/src/components/ChatInput/index.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { SmileOutlined, PaperClipOutlined, AudioOutlined, SendOutlined } from '@ant-design/icons'
-import { Button, Input } from 'antd'
+import { SmileOutlined, PaperClipOutlined, AudioOutlined, SendOutlined, DownloadOutlined } from '@ant-design/icons'
+import { Button, Input, Upload, Modal, List, Empty } from 'antd'
 
 import './ChatInput.scss'
 
 const ChatInput = () => {
 
     const [value, setValuue] = React.useState('');
+
+    const [visible, setVisible] = useState(false);
+    const [fileList, setFileList] = useState([]);
+
+    const handleUpload = () => {
+        // Здесь можно добавить логику для загрузки файлов на сервер
+        // Например, использовать fetch или axios для отправки файлов на сервер
+        console.log('Загрузка файлов:', fileList);
+        // После успешной загрузки файлов можно сбросить список файлов
+        setFileList([]);
+        // message.success('Файлы успешно загружены!');
+        setVisible(false);
+    };
+
+    const handleCancel = () => {
+        // При отмене загрузки файлов можно сбросить список файлов
+        setFileList([]);
+        setVisible(false);
+    };
+
+    const handleFileChange = (info) => {
+        let fileList = [...info.fileList];
+
+        // Ограничение на количество загружаемых файлов
+        fileList = fileList.slice(-5);
+
+        // Обновление списка файлов
+        fileList = fileList.map((file) => {
+            if (file.response) {
+                // Обработка ответа от сервера после загрузки файла
+                file.url = file.response.url;
+            }
+            return file;
+        });
+
+        setFileList(fileList);
+    };
 
     return (
         <div className='send'>
@@ -20,7 +57,32 @@ const ChatInput = () => {
                 onChange={(e) => setValuue(e.target.value)}
                 suffix={
                     <>
-                        <Button type="link" shape="circle" icon={<PaperClipOutlined />} />
+                        <Button type="link" onClick={() => setVisible(true)} icon={<PaperClipOutlined />} />
+                        <Modal
+                            title="Загрузка файлов"
+                            open={visible}
+                            onCancel={handleCancel}
+                            footer={[
+                                <Button key="cancel" onClick={handleCancel}>
+                                    Отмена
+                                </Button>,
+                                <Button key="upload" type="primary" onClick={handleUpload} disabled={fileList.length === 0}>
+                                    Загрузить
+                                </Button>,
+                            ]}
+                        >
+                            <Upload
+                                multiple
+                                onChange={handleFileChange}
+                                fileList={fileList}
+                                listType='picture'
+                            >
+                                <Button type="dashed" icon={<DownloadOutlined />}>Выберите файлы</Button>
+                            </Upload>
+                            {fileList.length === 0 ? (
+                                <Empty description="Нет файлов" />
+                            ) : (null)}
+                        </Modal>
                         {value
                             ? <Button type="link" shape="circle" icon={<SendOutlined />} />
                             : <Button type="link" shape="circle" icon={<AudioOutlined />} />
